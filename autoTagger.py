eca37e96c1efc3e06c69c9877041b4d95ecd2804
jfrausto7 <jaf25jaf@gmail.com>
2020-05-24 13:52:56 -0600

    Fixed bugs in autoTagger
    Caught StaleElementException for program overall. Also improved efficiency up to nearly 400%. Also cleaned up script aesthetically and included progress output in console for user.

--- autoTagger.py
+++ autoTagger.py
@@ -5,19 +5,22 @@ import sys
 
 import selenium
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import re
+
+from selenium.webdriver.support.wait import WebDriverWait
 
 """The following are the different helper methods used in the main script"""
 
 def inputPhase():
     # get input from user
-    email = input("What's your email?")
-    pw = input("What's your password?")
-    date = input("Enter date in MM/DD/YYYY format:")
+    email = input("What's your email? ")
+    pw = input("What's your password? ")
+    date = input("Enter date in MM/DD/YYYY format: ")
 
     info = checkInput(email,pw, date)   # loops through and checks input
 
@@ -35,6 +38,8 @@ def checkInput(email,pw, date):
     return [email,pw,date]
 
 def loginPhase():
+    # notify user of progress
+    print("Loading information...")
     # login phase
     login_button = browser.find_elements_by_id("Google")
     login_button.pop()
@@ -47,8 +52,9 @@ def loginPhase():
     try:
         email_box = browser.find_element_by_class_name("whsOnd").send_keys(email + Keys.ENTER)
     except selenium.common.exceptions.NoSuchElementException:
-        time.sleep(1)
-        email_box = browser.find_element_by_class_name("whsOnd").send_keys(email + Keys.ENTER)
+        wait = WebDriverWait(browser, 10)
+        email_box = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "whsOnd")))
+        email_box.send_keys(email + Keys.ENTER)
 
     time.sleep(2)  # slight waiting period
     # pw
@@ -58,11 +64,12 @@ def getPage():
     # go to date
     try:
         fromDate = browser.find_element_by_name("custSegfrom")
+        fromDate.clear()
+        fromDate.send_keys(date)
     except:
         browser.quit()
         print("You did not put in the right password. Please restart program")
-    fromDate.clear()
-    fromDate.send_keys(date)
+
     toDate = browser.find_element_by_name("custSegto")
     toDate.clear()
     toDate.send_keys(date + Keys.ENTER)
@@ -71,27 +78,40 @@ def getPage():
 
     time.sleep(2.4)
     browser.set_window_size(5000, 5000)
+    browser.set_window_position(2000, 2000)  # hides window
+
+
+
+def beginTags():
+
+    # drop-down fill-ins to set up tags
+    list = browser.find_elements_by_class_name("ddl-customerSegmentation-agent")
+    agents = []
+    while list:
+        agents.append(list.pop())
+    return agents
 
 def autoTag(bugs):
 
     # get max pages
-    maxPages = int(browser.find_element_by_class_name("pagination").get_attribute("data-pagecount"))
+    wait = WebDriverWait(browser, 10)
+    maxi = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "pagination")))
+    maxPages = int(maxi.get_attribute("data-pagecount"))
     currPage = 1
 
     while currPage <= maxPages:
 
-        # drop-down fill-ins
-        list = browser.find_elements_by_class_name("ddl-customerSegmentation-agent")
-        agents = []
-        while list:
-            agents.append(list.pop())
+        # print current page to terminal
+        print("Filling tags for page " + str(currPage) + ".")
 
-        # Create the object for Action Chains
-        actions = ActionChains(browser)
+        # Setup new page attributes
         scroll = 0
         count = 0
         past_loc = 0
         curr_loc = 0
+
+        agents = beginTags()
+
 
         while agents:
             count += 1
@@ -100,22 +120,15 @@ def autoTag(bugs):
                 curr_loc = agent.location.get('y')
                 select = Select(agent)
                 current = select.first_selected_option.text
-            except selenium.common.exceptions.ElementNotInteractableException:
+            except:
+                time.sleep(1.5)
+                beginTags()
                 continue
-            except selenium.common.exceptions.NoSuchElementException:
-                time.sleep(3)
-                curr_loc = agent.location.get('y')
-                select = Select(agent)
-                current = select.first_selected_option.text
-            except selenium.common.exceptions.StaleElementReferenceException:
-                time.sleep(3)
-                curr_loc = agent.location.get('y')
-                select = Select(agent)
-                current = select.first_selected_option.text
 
 
-            scroll += curr_loc - past_loc
+            scroll += curr_loc - past_loc   # get scroll value
 
+            # scroll based on current order. helps with debugging for some reason
             if count > 6:
                 browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
             else:
@@ -134,33 +147,26 @@ def autoTag(bugs):
                                 bugs += 1
                                 continue
             except:
-                time.sleep(1.25)
-                for option in select.options:
-                    if option.text == email:
-                        if current == "unassigned":     # only assigns agents to ones not previously assigned to!
-                            # try-catch block for weird exceptions
-                            try:
-                                select.select_by_visible_text(email)
-                                break
-                            except:
-                                print("bug detected")
-                                bugs += 1
-                                continue
+                time.sleep(1.5)
+                beginTags()
+                continue
 
 
             try:
                 past_loc = agent.location.get('y')
-            except selenium.common.exceptions.ElementNotInteractableException or selenium.common.exceptions.WebDriverException:
+            except:
+                time.sleep(1.5)
+                beginTags()
                 continue
 
         currPage += 1
         if currPage <= maxPages:
             # next page click
-            next = browser.find_element_by_link_text(str(currPage))
+            wait = WebDriverWait(browser, 10)
+            next = wait.until(EC.presence_of_element_located((By.LINK_TEXT, str(currPage))))
             browser.execute_script("arguments[0].click();", next)
             browser.execute_script("window.scrollTo(0,0)")
-            time.sleep(1.75)
-
+            time.sleep(1)
 
 
 """The main script"""
@@ -184,7 +190,7 @@ def resource_path(relative_path):
 
 browser = webdriver.Chrome(resource_path('./driver/chromedriver'))
 browser.maximize_window()
-# browser.set_window_position(2000,2000)
+browser.set_window_position(2000,2000)  # hides window
 
 # go to bacon website
 browser.get("https://bacon.signs.com/CustomerServicePortal/CustomerSegmentationIndex")
@@ -206,3 +212,5 @@ autoTag(bugs)
 browser.quit()  # close the browser
 
 print("Done! Caught " + str(bugs) + " bugs.")
+
+time.sleep(30)   # sleep for half a minute, then close program.
